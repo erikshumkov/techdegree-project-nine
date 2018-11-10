182e9e114d962df681d46495692cb026617c69a6
Erik Shumkov <erikshumkov@gmail.com>
2018-11-10 08:10:12 +0100

    Almost done with the project

--- js/addcharts.js
+++ js/addcharts.js
@@ -1,8 +1,29 @@
-var ctx = document.getElementById("areaChart").getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+
+window.onresize = () => reSize();
+
+// Change chart font size depending on the window width.
+function reSize() {
+  if (window.innerWidth < 1023) {
+    Chart.defaults.global.defaultFontSize = 12;
+  }
+
+  if (window.innerWidth >= 1024) {
+    Chart.defaults.global.defaultFontSize = 14;
+  }
+
+  if (window.innerWidth >= 1200) {
+    Chart.defaults.global.defaultFontSize = 17;
+  }
+};
+
+reSize();
+
+var firstChart = document.querySelector("#firstChart").getContext('2d');
 var ctx2 = document.getElementById("dailyTraffic").getContext('2d');
 var ctx3 = document.getElementById("mobileDoughnut").getContext('2d');
 
-var areaChartTraffic = new Chart(ctx, {
+var chartOne = new Chart(firstChart, {
     type: 'line',
     data: {
         datasets: [{
@@ -10,8 +31,10 @@ var areaChartTraffic = new Chart(ctx, {
             lineTension: 0,
             backgroundColor: 'rgba(116, 119, 191, 0.3)',
             borderColor: 'rgba(116, 119, 191,1)',
-            pointBackgroundColor: 'white',
-            pointBorderWidth: 1.5,
+            pointBackgroundColor: 'black',
+            pointBorderWidth: 1.8,
+            pointRadius: 4,
+            pointHoverBorderColor: 'rgba(116, 119, 191,0.7)',
             borderWidth: 0.5
         }]
     },
@@ -26,16 +49,23 @@ var areaChartTraffic = new Chart(ctx, {
         },
         scales: {
             xAxes: [{
+              gridLines: {
+                  color: 'rgba(255, 255, 255, .2)'
+              },
               type: 'category',
-              labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+              labels: ["1-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-52"]
             }],
             yAxes: [{
+                gridLines: {
+                    color: 'rgba(255, 255, 255, .2)'
+                },
                 ticks: {
                     beginAtZero:false,
                     maxTicksLimit: 5,
-                    min: 250,
+                    min: 500,
                     max: 2500,
-                    stepSize: 500
+                    stepSize: 500,
+                    stacked: true
                 }
             }]
         }
@@ -49,20 +79,33 @@ var dailyTrafficChart = new Chart(ctx2, {
         datasets: [{
             data: [75, 100, 175, 125, 225, 205, 105],
             lineTension: 0,
-            backgroundColor: 'rgba(116, 119, 191, 1)',
+            backgroundColor: 'rgba(116, 119, 191, 0.8)',
+            hoverBackgroundColor: 'rgba(116, 119, 191, 1)',
             borderColor: 'rgba(116, 119, 191,1)',
             borderWidth: 0.5
         }]
     },
     options: {
         legend: {
-          display: false,
+          display: false
         },
         scales: {
+            xAxes: [{
+                gridLines: {
+                    color: 'rgba(255, 255, 255, .3)'
+            }
+            }],
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
-                }
+                    beginAtZero: true,
+                    maxTicksLimit: 5,
+                    stepSize: 50,
+                    max: 300,
+                    min: 0
+                },
+                gridLines: {
+                    color: 'rgba(255, 255, 255, .3)'
+            }
             }]
         }
     }
@@ -75,21 +118,245 @@ var mobileDoughnutChart = new Chart(ctx3, {
         datasets: [{
             data: [65, 18, 17],
             lineTension: 0,
-            backgroundColor: ['rgba(116, 119, 191, 1)',
-                              'rgba(129,201,143,1)',
-                              'rgba(116,177,191,1)'
+            backgroundColor: ['rgba(116, 119, 191, 0.7)',
+                              'rgba(129, 201, 143, 0.7)',
+                              'rgba(116, 177, 191, 0.7)'
                              ],
-            borderColor: ['rgba(116, 119, 191, 1)',
-                          'rgba(129,201,143,1)',
-                          'rgba(116,177,191,1)'
-                         ],
-            borderWidth: 0.5
+            hoverBackgroundColor: ['rgba(116, 119, 191, 0.9)',
+                                   'rgba(129, 201, 143, 0.9)',
+                                   'rgba(116, 177, 191, 0.9)'
+                                 ],
+            borderWidth: 0
         }]
     },
     options: {
         legend: {
           display: true,
           position: 'right'
-        }
+        },
+        rotation: -25
     }
+});
+
+
+const hour = document.querySelector("#hour3");
+const day = document.querySelector("#day3");
+const week = document.querySelector("#week3");
+const month = document.querySelector("#month3");
+const clickMenu = document.querySelector(".switch-toggle");
+
+clickMenu.addEventListener("click", (event) => {
+
+  if(event.target.tagName == "INPUT") {
+    if(hour.checked) {
+
+        chartOne.destroy();
+        chartOne = new Chart(firstChart, {
+            type: 'line',
+            data: {
+                datasets: [{
+                    data: [10, 10, 60, 30, 10, 10, 50, 20, 15, 19, 10, 28, 40, 70, 50, 20, 17, 10, 11, 20, 15, 23, 24, 25],
+                    lineTension: 0,
+                    backgroundColor: 'rgba(116, 119, 191, 0.3)',
+                    borderColor: 'rgba(116, 119, 191,1)',
+                    pointBackgroundColor: 'black',
+                    pointBorderWidth: 2,
+                    pointRadius: 4,
+                    pointHoverBorderColor: 'rgba(116, 119, 191,0.7)',
+                    borderWidth: 0.5
+                }]
+            },
+            options: {
+                legend: {
+                  display: false,
+                },
+                plugins: {
+                    filler: {
+                        propagate: true
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                      gridLines: {
+                          color: 'rgba(255, 255, 255, .2)'
+                      },
+                      type: 'category',
+                      labels: ["00:00 AM", "01:00 AM", "02:00 AM", "03:00 AM", "04:00 AM", "05:00 AM", "06:00 AM", "07:00 AM", "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "13:00 PM", "14:00 PM", "15:00 PM", "16:00 PM", "17:00 PM", "18:00 PM", "19:00 PM", "20:00 PM", "21:00 PM", "22:00 PM", "23:00 PM"],
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: 'rgba(255, 255, 255, .2)'
+                        },
+                        ticks: {
+                            beginAtZero:true,
+                            min: 0,
+                            max: 300,
+                            stepSize: 50,
+                            stacked: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+
+    if(day.checked) {
+      chartOne.destroy();
+
+      chartOne = new Chart(firstChart, {
+          type: 'line',
+          data: {
+              datasets: [{
+                  data: [100, 175, 125, 225, 205, 105, 75],
+                  lineTension: 0,
+                  backgroundColor: 'rgba(116, 119, 191, 0.3)',
+                  borderColor: 'rgba(116, 119, 191,1)',
+                  pointBackgroundColor: 'black',
+                  pointBorderWidth: 1.8,
+                  pointRadius: 4,
+                  pointHoverBorderColor: 'rgba(116, 119, 191,0.7)',
+                  borderWidth: 0.5
+              }]
+          },
+          options: {
+              legend: {
+                display: false,
+              },
+              plugins: {
+                  filler: {
+                      propagate: true
+                  }
+              },
+              scales: {
+                  xAxes: [{
+                    gridLines: {
+                        color: 'rgba(255, 255, 255, .2)'
+                    },
+                    type: 'category',
+                    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+                  }],
+                  yAxes: [{
+                      gridLines: {
+                          color: 'rgba(255, 255, 255, .2)'
+                      },
+                      ticks: {
+                          beginAtZero:true,
+                          min: 0,
+                          max: 300,
+                          stepSize: 50,
+                          stacked: true
+                      }
+                  }]
+              }
+          }
+      });
+    }
+
+    if(week.checked) {
+
+        chartOne.destroy();
+
+        chartOne = new Chart(firstChart, {
+            type: 'line',
+            data: {
+                datasets: [{
+                    data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+                    lineTension: 0,
+                    backgroundColor: 'rgba(116, 119, 191, 0.3)',
+                    borderColor: 'rgba(116, 119, 191,1)',
+                    pointBackgroundColor: 'black',
+                    pointBorderWidth: 2,
+                    pointRadius: 4,
+                    pointHoverBorderColor: 'rgba(116, 119, 191,0.7)',
+                    borderWidth: 0.5
+                }]
+            },
+            options: {
+                legend: {
+                  display: false,
+                },
+                plugins: {
+                    filler: {
+                        propagate: true
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                      gridLines: {
+                          color: 'rgba(255, 255, 255, .2)'
+                      },
+                      type: 'category',
+                      labels: ["1-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-52"],
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: 'rgba(255, 255, 255, .2)'
+                        },
+                        ticks: {
+                            beginAtZero:false,
+                            maxTicksLimit: 5,
+                            min: 500,
+                            max: 2500,
+                            stepSize: 500,
+                            stacked: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+
+    if(month.checked) {
+        chartOne.destroy();
+
+        chartOne = new Chart(firstChart, {
+            type: 'line',
+            data: {
+                datasets: [{
+                    data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 1000],
+                    lineTension: 0,
+                    backgroundColor: 'rgba(116, 119, 191, 0.3)',
+                    borderColor: 'rgba(116, 119, 191,1)',
+                    pointBackgroundColor: 'black',
+                    pointBorderWidth: 2,
+                    pointRadius: 4,
+                    pointHoverBorderColor: 'rgba(116, 119, 191,0.7)',
+                    borderWidth: 0.5
+                }]
+            },
+            options: {
+                legend: {
+                  display: false,
+                },
+                plugins: {
+                    filler: {
+                        propagate: true
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                      gridLines: {
+                          color: 'rgba(255, 255, 255, .2)'
+                      },
+                      type: 'category',
+                      labels: ["January", "Febuary", "Mars", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: 'rgba(255, 255, 255, .2)'
+                        },
+                        ticks: {
+                            beginAtZero:true,
+                            min: 0,
+                            max: 2500,
+                            stepSize: 500,
+                            stacked: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+  }
+
 });
---
+++ js/app.js
@@ -0,0 +1,279 @@
+// Application ****************** //
+// ****************************** //
+
+
+// Scriptfel i IE. Fixa.
+
+// Sticky navbar from https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+// When the page get scrolled, use the stickyNav function.
+window.onscroll = () => stickyNav();
+
+const navigation = document.querySelector(".navigation--sidebar");
+const sticky = navigation.offsetTop;
+const mainContent = document.querySelector(".main-content");
+
+// When the window gets scrolled to the right position, the sticky navbar gets activated.
+// The sticky-nav class gets removed when it's not in the right position.
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navigation.classList.add("sticky-nav");
+
+    if(window.innerWidth >= 1024) {
+      mainContent.style.paddingLeft = "75px";
+      }
+  } else {
+    navigation.classList.remove("sticky-nav");
+
+    if(window.innerWidth >= 1024) {
+      mainContent.style.paddingLeft = "0px";
+      }
+  }
+  if(window.innerWidth < 1024) {
+    mainContent.style.paddingLeft = "0px";
+  }
+}
+
+// function newSize() {
+//   if(window.innerWidth < 1024) {
+//     mainContent.style.paddingLeft = "0px";
+//     }
+// }
+//
+// window.onresize = () => newSize();
+
+
+// Open / Close Notification box
+const bell = document.querySelector('.bell');
+const exitNote = document.querySelector('.exit-box');
+const numbersNote = document.querySelector('.number');
+
+function openCloseNotifications() {
+    const box = document.querySelector(".box");
+    if (box.style.display === "none") {
+        box.style.display = "block";
+        numbersNote.style.display = "none";
+    } else {
+        box.style.display = "none";
+    }
+}
+
+bell.addEventListener("click", function(e) {
+  openCloseNotifications();
+});
+
+exitNote.addEventListener("click", function(e) {
+  openCloseNotifications();
+});
+
+// Close alert
+const times = document.querySelector('.alert .fas');
+const divAlert = document.querySelector('.alert');
+
+times.addEventListener('click', () => {
+  divAlert.style.display = 'none';
+});
+
+// Send button, use JS to submit the form and display a success message.
+
+let counter = 0;
+const searchUser = document.querySelector("#searchuser");
+const textArea = document.querySelector("#message");
+const form = document.querySelector("form.message--user");
+let p = document.createElement('p');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  // Keep track of number of missed form fields with counter.
+  counter = 0;
+
+  // If no name or no message is entered
+  if (searchUser.value === "") {
+    counter++;
+    searchUser.style.borderColor = "red";
+  } else {
+    searchUser.style.borderColor = "lightgrey";
+  }
+
+  if (textArea.value === "") {
+    counter++;
+    textArea.style.borderColor = "red";
+  } else {
+    textArea.style.borderColor = "lightgrey";
+  }
+
+   if( searchUser.value === "" ||
+   			 textArea.value === "" ) {
+
+           p.textContent = `${counter} field(s) requires input. Please fill out and try again.`;
+           p.className = "error-alert";
+           form.appendChild(p);
+  }
+
+  // Add a success message if form is complete.
+  if (searchUser.value !== ""
+      &&
+        textArea.value !== "") {
+        if(document.querySelector(".message-settings fieldset p")) {
+          form.removeChild(p);
+        }
+    searchUser.value = "";
+    textArea.value = "";
+    form.appendChild(p);
+    p.className = "success-alert";
+    p.textContent = `Message was successfully sent!`;
+  }
+
+});
+
+
+// Members array for autocomplete functionality.
+const users = [
+  'Dale Byrd',
+  'Victoria Chambers',
+  'Dawn Wood',
+  'Dan Oliver'
+];
+
+// Autocomplete functionality from:
+// https://www.w3schools.com/howto/howto_js_autocomplete.asp
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  let currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      let div, divMatch, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) {
+        return false;
+      }
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      div = document.createElement("DIV");
+      div.setAttribute("id", this.id + "autofill-list");
+      div.setAttribute("class", "autofill-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(div);
+      /*for each item in the array...*/
+      for ( let i = 0; i < arr.length; i++ ) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substring(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          divMatch = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          divMatch.innerHTML = "<strong>" + arr[i].substring(0, val.length) + "</strong>";
+          divMatch.innerHTML += arr[i].substring(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          divMatch.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              divMatch.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+                closeAllLists();
+              });
+              div.appendChild(divMatch);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      let x = document.getElementById(this.id + "autofill-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.key == "ArrowDown") {
+        /*If the arrow DOWN key is pressed,
+                increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.key == "ArrowUp") {
+        /*If the arrow UP key is pressed,
+                decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.key == "Enter") {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length -1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autofill-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for ( let i = 0; i < x.length; i++ ) {
+      x[i].classList.remove("autofill-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+    let x = document.getElementsByClassName("autofill-items");
+    for ( let i = 0; i < x.length; i++ ) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function(e) {
+    closeAllLists(e.target);
+  });
+}
+
+autocomplete(searchUser, users);
+// Autocomplete end.
+
+// Use local storage to save settings
+let onoffOne = document.querySelector('.switch-one input');
+let onoffTwo = document.querySelector('.switch-two input');
+let timezone = document.querySelector('.settings select');
+const buttonSave = document.getElementById('save');
+const buttonCancel = document.getElementById('cancel');
+
+// Save localStorage with #save button, onclick in index.html
+function saveSettings() {
+  localStorage.setItem('onoffOne', JSON.stringify(onoffOne.checked));
+  localStorage.setItem('onoffTwo', JSON.stringify(onoffTwo.checked));
+  localStorage.setItem('select', JSON.stringify(timezone.selectedIndex));
+}
+
+// Load localStorage and get values
+function loadStorage() {
+  const checked = JSON.parse(localStorage.getItem('onoffOne'));
+  const checkedTwo = JSON.parse(localStorage.getItem('onoffTwo'));
+  const selectValue = JSON.parse(localStorage.getItem('select'));
+
+  if (checked == false) {
+    onoffOne.removeAttribute("checked");
+  }
+  if (checkedTwo == false) {
+    onoffTwo.removeAttribute("checked");
+  }
+  if (selectValue > 0) {
+    timezone.selectedIndex = selectValue;
+  }
+}
+
+// Load localStorage
+loadStorage();
+
+// Clear storage with #cancel button, onclick in index.html
+function removeStorage() {
+  localStorage.clear();
+}
